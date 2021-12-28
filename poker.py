47cdf3a281c39fb547838740690f913c6f36afdd
TeddyBear714 <ze19163@bristol.ac.uk>
2021-12-28 22:48:15 +0200

    4th

--- poker.py
+++ poker.py
@@ -20,3 +20,38 @@ p1.set_money(1000)
 print(p1.name)
 print("\n")
 print(p1.money)
+
+class Card:
+    def __init__(self,number,suit):
+        self.number = number
+        self.suit = suit
+
+class Table:
+    def __init__(self,number_of_players):
+        self.number_of_players = number_of_players
+        self.deck1 = []
+        for suit in ["spades","clubs","diamonds","hearts"]:
+            for i in range(1,14):
+                if i==1:
+                    self.deck1.append(Card("Ace",suit))
+                    continue
+                if i==11:
+                    self.deck1.append(Card("Jack",suit))
+                    continue
+                if i==12:
+                    self.deck1.append(Card("Queen",suit))
+                    continue
+                if i==13:
+                    self.deck1.append(Card("King",suit))
+                    continue
+                self.deck1.append(Card(str(i),suit))
+        
+                
+t = Table(2)
+
+for i in t.deck1:
+    print([i.number,i.suit])
+
+#print(t.deck1)
+    #def shuffler(self):
+            
